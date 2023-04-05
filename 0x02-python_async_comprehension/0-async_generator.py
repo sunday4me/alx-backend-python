595ca8e70bcd8205ced6cffd83e05047aaf5b921
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-05 06:41:29 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module - async_generator
+"""
+import asyncio
+import random
+from typing import Generator
 
+
+async def async_generator() -> Generator[float, None, None]:
+    """async generator"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
