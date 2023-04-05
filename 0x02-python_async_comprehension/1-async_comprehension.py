8efa2450f3e62f7fdfd2e005809791ada2d7397e
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-05 14:24:51 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -13,4 +13,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
     results = [i async for i in async_generator()]
-    return 
+    return results 
