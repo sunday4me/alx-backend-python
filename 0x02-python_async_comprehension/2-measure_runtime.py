895731517e58d06cf3869122493d3d781612976f
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-05 06:43:28 +0100

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module - measure_runtime
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measures runtime of a coroutine"""
+    start_time = time.time()
+
+    loop = asyncio.get_event_loop()
+    delay = await asyncio.gather(async_comprehension(), async_comprehension(),
+                                 async_comprehension(), async_comprehension())
+
+    end_time = time.time()
+
+    return end_time - start_time
 
