18ebf593f266e955630812608244dd2d5d6b0dc8
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-05 06:42:32 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module - async_comprehension
+"""
+from typing import List
 
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async comprehension"""
+    result = [i async for i in async_generator()]
+    return result
